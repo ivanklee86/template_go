aaff1154c09e2ebf0df3914f60929399ee3a8c77
Ivan Lee <ivanklee86@gmail.com>
2025-02-23 11:32:08 +0000

    Standardize some docs.

--- .devcontainer/post_install.sh
+++ .devcontainer/post_install.sh
@@ -5,19 +5,23 @@ set -ex
 rm -rf /home/vscode/.uv_cache | true
 mkdir /home/vscode/.uv_cache | true
 uv venv
+
+# Install pre-commit hooks
 uv pip install -r requirements.txt
 uv run pre-commit install
 
-# Configure git
+# Configure git...
+
+# ...to do GPG code signing locally.
 if [ "$CODESPACES" != "true" ]; then
     echo "Running locally, configure gpg."
-
     git config --global gpg.program gpg2
     git config --global commit.gpgsign true
 else
     echo "Running in codespaces."
 fi
 
+# ...to automatically setup remote branches.
 git config --global --add --bool push.autoSetupRemote true
 
 # Woohoo!
